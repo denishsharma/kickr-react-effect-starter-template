48359a72531f8bf1fb61232652b72fb66058f40a
Denish Sharma <denishcommon@gmail.com>
2025-04-15 18:03:10 +0530

    refactor: correct filename spelling

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import router from '~/core/router/tanstack'
 import head from '~/core/unhead/head'
-import '~/assets/styles/gloabl.css'
+import '~/assets/styles/global.css'
 
 createRoot(document.getElementById('__app_root__')!).render(
   <StrictMode>
